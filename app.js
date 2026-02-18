dcd93606f2d367fbf411f5100d028d12ad40c669
yutatakabatake <yutatakabatake@gmail.com>
2026-02-18 20:18:06 +0900

    Add fetching

--- app.js
+++ app.js
@@ -3,10 +3,17 @@ const username = process.argv[2];
 async function getUserActivity(username) {
     try {
         const response = await fetch(`https://api.github.com/users/${username}`);
-        console.log(response);
-        return response;
+        const status = response.status;
+        const ok = response.ok;
+
+        if (!ok) {
+            throw new Error(`GitHub API error ${status}`);
+        }
+
+        const events = await response.json();
+
     } catch (error) {
-        console.error("Error fetch: ", error);
+        console.error("Error getUserActivity : ", error);
     }
 }
 
